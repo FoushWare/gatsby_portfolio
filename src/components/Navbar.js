0c451573471a60e0014ef181cf364daea3e6487e
foushware <mr.fouad992@gmail.com>
2021-04-20 21:40:13 +0200

    add link to the webLog

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "gatsby"
 import logo from "../assets/logo.svg"
 import { FaAlignRight } from "react-icons/fa"
 import PageLinks from "../constants/links"
@@ -6,7 +7,9 @@ const Navbar = ({ toggleSidebar }) => {
   return <nav className="navbar">
     <div className="nav-center">
       <div className="nav-header">
-        <img src={logo} alt="logo" />
+        <Link to="/">
+          <img src={logo} alt="logo" />
+        </Link>
         <button type="button" className="toggle-btn" onClick={toggleSidebar}>
           <FaAlignRight></FaAlignRight>
         </button>
